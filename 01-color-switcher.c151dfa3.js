74a6b29b305300a7396902b2731acdb015786cfc
helga-khols <97860059+helga-khols@users.noreply.github.com>
2022-08-21 21:31:16 +0000

    Deploying to gh-pages from @ helga-khols/goit-js-hw-09@bb0fdbecb3f5f89f4f18f346588b917650453ebb 🚀

---
+++ 01-color-switcher.c151dfa3.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.start.addEventListener("click",(function(){t.start.setAttribute("disabled",!0),t.stop.removeAttribute("disabled"),e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){t.start.removeAttribute("disabled"),t.stop.setAttribute("disabled",!0),clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.c151dfa3.js.map
